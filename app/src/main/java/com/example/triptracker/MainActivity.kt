7b9600f63c108579d264823cde3570af507ee1bf
Barghorn Jérémy <jeremy.barghorn@epfl.ch>
2024-03-19 21:19:13 +0100

    Add ci and refactor code template

--- app/src/main/java/com/example/triptracker/MainActivity.kt
+++ app/src/main/java/com/example/triptracker/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
       TripTrackerTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("TripTracker")
+          Greeting("TripTracker !")
         }
       }
     }
