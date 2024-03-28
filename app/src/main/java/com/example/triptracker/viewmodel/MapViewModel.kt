1a2b6d1a8ad7a1fc5fe18494dc9fac8cd110ee4c
Cléo Renaud <73248361+cleorenaud@users.noreply.github.com>
2024-03-28 12:58:24 +0100

    Update MapViewModel.kt
    Updated the comment for the reverseDecode function

--- app/src/main/java/com/example/triptracker/viewmodel/MapViewModel.kt
+++ app/src/main/java/com/example/triptracker/viewmodel/MapViewModel.kt
@@ -17,10 +17,10 @@ class MapViewModel {
 
   /**
    * Reverse decodes the location to get the city name.
+   * On success update the cityNameState at the top of the screen
    *
-   * @param lat latitude of the location
-   * @param lon longitude of the location On success update the cityNameState on the top of the
-   *   screen
+   * @param lat : latitude of the location
+   * @param lon : longitude of the location
    */
   fun reverseDecode(lat: Float, lon: Float) {
     geocoder.reverseDecode(lat, lon) { cityName -> cityNameState.value = cityName }
