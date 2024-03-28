979bb4d2c9819ef4dc84e51f3570a3bc53978547
jchaverot <jeremy.chaverot@epfl.ch>
2024-03-28 09:40:23 +0100

    create the 4 main app fragments and compose them with MainActivity for test

--- app/src/main/java/com/example/triptracker/MainActivity.kt
+++ app/src/main/java/com/example/triptracker/MainActivity.kt
@@ -16,8 +16,11 @@ import com.example.triptracker.view.Navigation
 import com.example.triptracker.view.Route
 import com.example.triptracker.view.theme.TripTrackerTheme
 
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
+  /*override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
       // Set the trip tracker theme
@@ -36,6 +39,34 @@ class MainActivity : ComponentActivity() {
           }
         }
       }
+    }*/
+    override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_main)
+
+      val firstFragment=ProfileFragment()
+      val secondFragment=MapsFragment()
+      val thirdFragment=RecordFragment()
+      val fourthFragment=ProfileFragment()
+
+      setCurrentFragment(firstFragment)
+
+      bottomNavigationView.setOnNavigationItemSelectedListener {
+        when(it.itemId){
+          R.id.home->setCurrentFragment(firstFragment)
+          R.id.person->setCurrentFragment(secondFragment)
+          R.id.settings->setCurrentFragment(thirdFragment)
+
+        }
+        true
+      }
+
     }
+
+    private fun setCurrentFragment(fragment:Fragment)=
+      supportFragmentManager.beginTransaction().apply {
+        replace(R.id.flFragment,fragment)
+        commit()
+      }
   }
 }
