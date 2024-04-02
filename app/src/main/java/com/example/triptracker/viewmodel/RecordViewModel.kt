82a56d8b1664951ce4fa6f11c1f5d93e1f1b5c38
Loic Misenta <loic.misenta@epfl.ch>
2024-04-02 13:58:04 +0200

    map + button + timer + tracking + path + animation work + cleaning

--- app/src/main/java/com/example/triptracker/viewmodel/RecordViewModel.kt
+++ app/src/main/java/com/example/triptracker/viewmodel/RecordViewModel.kt
@@ -9,24 +9,37 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+/**
+ * ViewModel for recording trips.
+ * This ViewModel is responsible for managing the state of a trip recording, including start time, end time, pause status, and the list of LatLng points.
+ */
 class RecordViewModel {
 
+    // Start time of the recording
     private val startTime = mutableLongStateOf(0L)
+    // End time of the recording
     private val endTime = mutableLongStateOf(0L)
+    // Boolean state representing whether the recording is paused
     val isPaused = mutableStateOf(false)
 
+    // Private mutable list of LatLng points
     private var _latLongList = mutableListOf<LatLng>()
+    // Public immutable list of LatLng points
     val latLongList: List<LatLng>
         get() = _latLongList
 
-    private var currentLatLng = mutableStateOf(LatLng(0.0, 0.0))
-
+    /**
+     * Starts the recording.
+     * Sets the start time to the current time.
+     */
     fun startRecording() {
         startTime.longValue = System.currentTimeMillis()
-        Log.d("start time", startTime.longValue.toString())
-        //trackLocation()
     }
 
+    /**
+     * Stops the recording.
+     * Sets the end time to the current time and logs the data collected.
+     */
     fun stopRecording() {
         endTime.longValue = System.currentTimeMillis()
         // TODO do something with the data
@@ -34,26 +47,46 @@ class RecordViewModel {
         //clear the list
     }
 
+    /**
+     * Resets the recording.
+     * Clears the start time, end time, and LatLng list.
+     */
     fun resetRecording() {
         startTime.longValue = 0L
         endTime.longValue = 0L
         _latLongList.clear()
     }
 
+    /**
+     * Pauses the recording.
+     * Sets the end time to the current time and sets the pause status to true.
+     */
     fun pauseRecording() {
         endTime.longValue = System.currentTimeMillis()
         isPaused.value = true
     }
 
+    /**
+     * Resumes the recording.
+     * Adjusts the start time based on the duration of the pause and sets the pause status to false.
+     */
     fun resumeRecording() {
         startTime.longValue += System.currentTimeMillis() - endTime.longValue
         isPaused.value = false
     }
 
+    /**
+     * Checks if the recording is ongoing.
+     * @return Boolean indicating whether the recording is ongoing.
+     */
     fun isRecording(): Boolean {
         return startTime.longValue != 0L
     }
 
+    /**
+     * Calculates the elapsed time of the recording.
+     * @return Long representing the elapsed time in milliseconds.
+     */
     fun getElapsedTime(): Long {
         return if (isRecording() && !isPaused.value) {
             val cur = System.currentTimeMillis()
@@ -67,37 +100,12 @@ class RecordViewModel {
         }
     }
 
-    fun getStartTime(): Long {
-        return startTime.longValue
-    }
-
+    /**
+     * Adds a LatLng point to the list.
+     * @param latLng The LatLng point to add.
+     */
     fun addLatLng(latLng: LatLng) {
         _latLongList.add(latLng)
     }
-
-    fun updateLatLng(latLng: LatLng) {
-        currentLatLng.value = latLng
-    }
-
-    @OptIn(DelicateCoroutinesApi::class)
-    private fun trackLocation() {
-        GlobalScope.launch {
-            while(isRecording()) {
-                // get current location
-                val currentLocation = currentLatLng.value
-
-                // add to arraylist
-                _latLongList.add(currentLocation)
-
-                Log.d("pos added", if(_latLongList.isNotEmpty()) _latLongList.last().toString() else "empty")
-                Log.d("list size", _latLongList.size.toString())
-                Log.d("waiting", "waiting for 1 second)")
-                // sleep for n seconds
-                delay(10000)
-                Log.d("waiting", "done waiting")
-            }
-        }
-    }
-
 
 }
