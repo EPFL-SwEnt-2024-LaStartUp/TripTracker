9327f70a8a3c0d0ca8af8918496a2771b22b8b92
Barghorn Jérémy <jeremy.barghorn@epfl.ch>
2024-04-03 23:55:12 +0200

    Ktfmt format for the CI and added some comments

--- app/src/main/java/com/example/triptracker/model/geocoder/NominatimApi.kt
+++ app/src/main/java/com/example/triptracker/model/geocoder/NominatimApi.kt
@@ -1,13 +1,13 @@
 package com.example.triptracker.model.geocoder
 
 import android.util.Log
+import java.io.IOException
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import org.json.JSONObject
-import java.io.IOException
 
 /**
  * Class containing all the nominatim API variants that describe a city to reverse decode the
