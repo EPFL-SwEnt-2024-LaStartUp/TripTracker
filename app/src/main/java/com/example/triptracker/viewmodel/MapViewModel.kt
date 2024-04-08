89b06960c6391b21437ff634093db29b5830db01
Barghorn Jérémy <jeremy.barghorn@epfl.ch>
2024-04-08 14:32:56 +0200

    Add the ItineraryRepository to the viewModel of the app

--- app/src/main/java/com/example/triptracker/viewmodel/MapViewModel.kt
+++ app/src/main/java/com/example/triptracker/viewmodel/MapViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.triptracker.viewmodel
 
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.triptracker.model.geocoder.NominatimApi
+import com.example.triptracker.model.itinerary.Itinerary
+import com.example.triptracker.model.location.Location
+import com.example.triptracker.model.repository.ItineraryRepository
+import com.google.android.gms.maps.model.LatLng
 
 /**
  * ViewModel for the MapOverview composable. It contains the city name state and the geocoder to
@@ -15,6 +21,10 @@ class MapViewModel {
   // state for the city name displayed at the top of the screen
   val cityNameState = mutableStateOf("")
 
+  private val repository = ItineraryRepository()
+
+  private val _pathList = MutableLiveData<Map<Location, List<LatLng>>>(emptyMap())
+  val itineraryList: LiveData<Map<Location, List<LatLng>>> = _pathList
   /**
    * Reverse decodes the location to get the city name. On success update the cityNameState at the
    * top of the screen
@@ -24,5 +34,13 @@ class MapViewModel {
    */
   fun reverseDecode(lat: Float, lon: Float) {
     geocoder.reverseDecode(lat, lon) { cityName -> cityNameState.value = cityName }
+  }
+
+  fun getAllPaths(){
+    repository.getAllItineraries { itineraries ->
+      for (itinerary in itineraries) {
+//        _itineraryList.value += //TODO add the list of LatLng points of the itinerary
+      }
+    }
   }
 }
