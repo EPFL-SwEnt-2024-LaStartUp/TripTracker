262dc6ccf0c6cb4d5a25924e88276bdbad95fc40
jchaverot <jeremy.chaverot@epfl.ch>
2024-04-10 14:47:33 +0200

    Create files for map tests

--- app/src/androidTest/java/com/example/triptracker/map/MapOverviewTest.kt
+++ app/src/androidTest/java/com/example/triptracker/map/MapOverviewTest.kt
@@ -1,8 +1,6 @@
 package com.example.triptracker.map
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.triptracker.viewmodel.MapViewModel
-import com.example.triptracker.view.map.MapOverview
 import java.util.Date
 import org.junit.Assert.assertEquals
 import org.junit.Test
