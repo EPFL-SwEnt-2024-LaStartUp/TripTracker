cf9f0538cf3bbcbaab80fd42e786cfa2fbbc8d01
jchaverot <jeremy.chaverot@epfl.ch>
2024-04-11 22:09:36 +0200

    Another test for the tests, but with preview since always ask for authorizations

--- app/src/androidTest/java/com/example/triptracker/map/MapTest.kt
+++ app/src/androidTest/java/com/example/triptracker/map/MapTest.kt
@@ -1,10 +1,17 @@
 package com.example.triptracker.map
 
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.triptracker.MainActivity
-import com.example.triptracker.view.map.MapOverview
+import com.example.triptracker.screens.LoginScreen
+import com.example.triptracker.view.map.MapOverviewPreview
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import io.github.kakaocup.compose.node.element.ComposeScreen
 import org.junit.Rule
@@ -13,7 +20,7 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MapTest : TestCase() {
-  @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+  @get:Rule val composeTestRule = createComposeRule()
 
   // The IntentsTestRule simply calls Intents.init() before the @Test block
   // and Intents.release() after the @Test block is completed. IntentsTestRule
@@ -21,10 +28,8 @@ class MapTest : TestCase() {
   @get:Rule val intentsTestRule = IntentsTestRule(MainActivity::class.java)
 
   @Test
-  fun titleAndButtonAreCorrectlyDisplayed() {
-    ComposeScreen.onComposeScreen<MapOverview>(composeTestRule) {
-      // Test the UI elements
-       { assertIsDisplayed() }
-    }
+  fun mapOverviewDisplaysCorrectly() {
+    composeTestRule.setContent { MapOverviewPreview() }
+    composeTestRule.onNodeWithText("Map").assertIsDisplayed()
   }
 }
