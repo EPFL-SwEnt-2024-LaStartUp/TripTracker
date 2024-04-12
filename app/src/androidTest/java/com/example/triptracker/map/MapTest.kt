3a6debc705309210cc50e102489884dc06edbc44
jchaverot <jeremy.chaverot@epfl.ch>
2024-04-12 17:27:09 +0200

    KTFmt reformat

--- app/src/androidTest/java/com/example/triptracker/map/MapTest.kt
+++ app/src/androidTest/java/com/example/triptracker/map/MapTest.kt
@@ -1,16 +1,10 @@
 package com.example.triptracker.map
 
 import android.content.Context
-import androidx.compose.runtime.remember
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.navigation.compose.rememberNavController
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.GrantPermissionRule
-import com.example.triptracker.view.Navigation
-import com.example.triptracker.view.map.MapOverview
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.junit.Rule
 import org.junit.Test
@@ -20,11 +14,9 @@ import org.junit.runner.RunWith
 val permissionRule: GrantPermissionRule =
     GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION)
 
-
 /**
- * Tests for MapOverview.
- * Still not working because of errors with undeterministic patterns when logging in and asking
- * for user location permission.
+ * Tests for MapOverview. Still not working because of errors with undeterministic patterns when
+ * logging in and asking for user location permission.
  */
 @RunWith(AndroidJUnit4::class)
 class MapTest : TestCase() {
@@ -36,25 +28,25 @@ class MapTest : TestCase() {
   // is deprecated, but it was MUCH faster than using IntentsRule in our tests
   // @get:Rule val intentsTestRule = IntentsTestRule(MainActivity::class.java)
 
-  //@Test
-  //fun mapOverviewDisplaysCorrectly() {
+  // @Test
+  // fun mapOverviewDisplaysCorrectly() {
   //  composeTestRule.setContent {
   //    val navController = rememberNavController()
   //    val navigation = remember(navController) { Navigation(navController) }
   //    MainActivity()
   //  }
   //  composeTestRule.onNodeWithText("Mountain View").assertIsDisplayed()
-  //}
+  // }
 
-  //@Test
-  //fun test2() {
+  // @Test
+  // fun test2() {
   //
   //  composeTestRule.setContent {
   //    val navController = rememberNavController()
   //    val navigation = remember(navController) { Navigation(navController) }
   //    MapOverview(navigation = navigation, context = appContext)
   //  }
-  //}
+  // }
 
   @Test
   fun emptyTest() {
