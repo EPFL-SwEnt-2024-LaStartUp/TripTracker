a01c6352ba06a937c391c7bad1f2d4608cbb05ba
Barghorn Jérémy <jeremy.barghorn@epfl.ch>
2024-04-16 23:13:23 +0200

    Changed some tests locaion

--- app/src/test/java/com/example/triptracker/NominatimGeocoderTest.kt
+++ app/src/test/java/com/example/triptracker/NominatimGeocoderTest.kt
@@ -1,16 +1,15 @@
 package com.example.triptracker
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.triptracker.model.geocoder.NominatimApi
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
+
 class NominatimGeocoderTest {
-  val geocoder = NominatimApi()
+
 
   @Test
   fun testDecode() {
+    val geocoder = NominatimApi()
     geocoder.decode("EPFL") { location ->
       assert(location.latitude == 46.519879)
       assert(location.longitude == 6.560632)
@@ -24,6 +23,7 @@ class NominatimGeocoderTest {
 
   @Test
   fun getCityTest() {
+    val geocoder = NominatimApi()
     geocoder.getCity(46.519053.toFloat(), 6.568287.toFloat()) { city -> assert(city == "Lausanne") }
 
     geocoder.getCity(0.0.toFloat(), 0.0.toFloat()) { city -> assert(city == "Unknown") }
@@ -31,6 +31,7 @@ class NominatimGeocoderTest {
 
   @Test
   fun getPOITest() {
+    val geocoder = NominatimApi()
     geocoder.getPOI(46.519053.toFloat(), 6.568287.toFloat()) { poi ->
       assert(poi.contains("École Polytechnique Fédérale de Lausanne"))
     }
