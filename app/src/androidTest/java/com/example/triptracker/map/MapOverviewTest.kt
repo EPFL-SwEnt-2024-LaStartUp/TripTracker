ce7db0cdfc839d4d49a3b68223eafba8a6c201ed
Barghorn Jérémy <jeremy.barghorn@epfl.ch>
2024-04-25 17:28:06 +0200

    Added some tests

--- app/src/androidTest/java/com/example/triptracker/map/MapOverviewTest.kt
+++ app/src/androidTest/java/com/example/triptracker/map/MapOverviewTest.kt
@@ -123,4 +123,51 @@ class MapOverviewTest : TestCase() {
     // Verify that the dependencies are properly initialized
     composeTestRule.onNodeWithTag("MapOverview").assertExists()
   }
+
+  @Test
+  fun testClickOnPath() {
+    val itineraryList = MockItineraryList().getItineraries()
+    every { mockViewModel.cityNameState.value } returns "Lyon"
+    every { mockViewModel.filteredPathList.value } returns
+        mapOf(itineraryList[0] to listOf<LatLng>(LatLng(0.0, 0.0)))
+    every { mockViewModel.selectedPolylineState.value } returns
+        MapViewModel.SelectedPolyline(itineraryList[0], LatLng(0.0, 0.0))
+
+    every { mockViewModel.displayPopUp.value } returns true
+
+    composeTestRule.setContent {
+      MapOverview(mapViewModel = mockViewModel, context = appContext, navigation = mockNavigation)
+    }
+
+    composeTestRule.onNodeWithTag("MapOverview").assertExists()
+    composeTestRule.onNodeWithTag("Map").assertExists()
+
+    // Verify that the dependencies are properly called
+    verify { mockViewModel.selectedPolylineState.value }
+  }
+
+  @Test
+  fun testClickOnPicture() {
+    val itineraryList = MockItineraryList().getItineraries()
+    every { mockViewModel.cityNameState.value } returns "Lyon"
+    every { mockViewModel.filteredPathList.value } returns
+        mapOf(itineraryList[0] to listOf<LatLng>(LatLng(0.0, 0.0)))
+    every { mockViewModel.selectedPolylineState.value } returns
+        MapViewModel.SelectedPolyline(itineraryList[0], LatLng(0.0, 0.0))
+    every { mockViewModel.selectedPin.value } returns itineraryList[0].pinnedPlaces[0]
+
+    every { mockViewModel.displayPopUp.value } returns false
+
+    every { mockViewModel.displayPicturesPopUp.value } returns true
+
+    composeTestRule.setContent {
+      MapOverview(mapViewModel = mockViewModel, context = appContext, navigation = mockNavigation)
+    }
+
+    composeTestRule.onNodeWithTag("MapOverview").assertExists()
+    composeTestRule.onNodeWithTag("Map").assertExists()
+
+    // Verify that the dependencies are properly called
+    verify { mockViewModel.selectedPolylineState.value }
+  }
 }
