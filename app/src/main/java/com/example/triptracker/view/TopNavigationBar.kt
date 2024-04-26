1b36930dcc08f175c486759252f2339b31ab9c53
jchaverot <jeremy.chaverot@epfl.ch>
2024-04-26 18:10:17 +0200

    Take into account Cleo's comment in the review of the PR

--- app/src/main/java/com/example/triptracker/view/TopNavigationBar.kt
+++ app/src/main/java/com/example/triptracker/view/TopNavigationBar.kt
@@ -33,53 +33,53 @@ import com.example.triptracker.view.theme.md_theme_orange
  * @param navigateUp The action to perform when the back button is clicked.
  * @param actions The actions to display on the top bar (like a save button).
  */
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun NavTopBar(
-    modifier: Modifier = Modifier,
-    title: String,
-    canNavigateBack: Boolean,
-    navigateUp: () -> Unit = {},
-    actions: @Composable () -> Unit = {}
-) {
-  if (canNavigateBack) {
-    TopAppBar(
-        title = { Text(text = title) },
-        actions = { actions() },
-        navigationIcon = {
-          IconButton(onClick = navigateUp) {
-            Icon(
-                imageVector = Icons.Default.ArrowBack,
-                contentDescription = "Back",
-                tint = md_theme_grey)
-          }
-        },
-        modifier = modifier)
-  } else {
-    TopAppBar(title = { Text(text = title) }, actions = { actions() }, modifier = modifier)
-  }
-}
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun NavTopBar(
+//    modifier: Modifier = Modifier,
+//    title: String,
+//    canNavigateBack: Boolean,
+//    navigateUp: () -> Unit = {},
+//    actions: @Composable () -> Unit = {}
+//) {
+//  if (canNavigateBack) {
+//    TopAppBar(
+//        title = { Text(text = title) },
+//        actions = { actions() },
+//        navigationIcon = {
+//          IconButton(onClick = navigateUp) {
+//            Icon(
+//                imageVector = Icons.Default.ArrowBack,
+//                contentDescription = "Back",
+//                tint = md_theme_grey)
+//          }
+//        },
+//        modifier = modifier)
+//  } else {
+//    TopAppBar(title = { Text(text = title) }, actions = { actions() }, modifier = modifier)
+//  }
+//}
 
 /** Preview for the NavTopBar to have an example. */
-@Preview
-@Composable
-fun NavTopBarPreview() {
-  NavTopBar(
-      title = stringResource(id = R.string.app_name),
-      canNavigateBack = true,
-      navigateUp = {},
-      actions = {
-        FilledTonalButton(
-            onClick = {},
-            colors = ButtonDefaults.filledTonalButtonColors(containerColor = md_theme_orange),
-            modifier = Modifier.width(90.dp).height(30.dp),
-        ) {
-          Text(
-              text = "Save",
-              fontSize = 14.sp,
-              fontFamily = Montserrat,
-              fontWeight = FontWeight.SemiBold,
-              color = Color.White)
-        }
-      })
-}
+//@Preview
+//@Composable
+//fun NavTopBarPreview() {
+//  NavTopBar(
+//      title = stringResource(id = R.string.app_name),
+//      canNavigateBack = true,
+//      navigateUp = {},
+//      actions = {
+//        FilledTonalButton(
+//            onClick = {},
+//            colors = ButtonDefaults.filledTonalButtonColors(containerColor = md_theme_orange),
+//            modifier = Modifier.width(90.dp).height(30.dp),
+//        ) {
+//          Text(
+//              text = "Save",
+//              fontSize = 14.sp,
+//              fontFamily = Montserrat,
+//              fontWeight = FontWeight.SemiBold,
+//              color = Color.White)
+//        }
+//      })
+//}
