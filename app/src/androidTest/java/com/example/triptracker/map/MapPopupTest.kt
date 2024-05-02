7471ee2ce7df1a4e9acd1665f9a81b7ba961dfec
Pol <polinboston@gmail.com>
2024-05-02 17:09:03 +0200

    changed

--- app/src/androidTest/java/com/example/triptracker/map/MapPopupTest.kt
+++ app/src/androidTest/java/com/example/triptracker/map/MapPopupTest.kt
@@ -1,8 +1,7 @@
 package com.example.triptracker.map
 
-import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -29,7 +28,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MapPopupTest {
 
-  @get:Rule val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+  @get:Rule val composeTestRule = createComposeRule()
 
   @RelaxedMockK private lateinit var mockViewModel: UserProfileViewModel
 
