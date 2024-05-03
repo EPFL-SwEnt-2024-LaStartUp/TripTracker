cba2105e285b532aed84b40f599753da4698bbf8
cleo <cleo.renaud@epfl.ch>
2024-05-03 09:04:03 +0200

    Update UserProfileFollowersTest.kt

--- app/src/androidTest/java/com/example/triptracker/userProfile/UserProfileFollowersTest.kt
+++ app/src/androidTest/java/com/example/triptracker/userProfile/UserProfileFollowersTest.kt
@@ -1,31 +1,32 @@
-//package com.example.triptracker.userProfile
+// package com.example.triptracker.userProfile
 //
-//import androidx.compose.ui.test.junit4.createComposeRule
-//import androidx.lifecycle.MutableLiveData
-//import androidx.test.ext.junit.runners.AndroidJUnit4
-//import com.example.triptracker.model.profile.UserProfile
-//import com.example.triptracker.model.repository.UserProfileRepository
-//import com.example.triptracker.screens.userProfile.UserProfileFollowersScreen
-//import com.example.triptracker.view.Navigation
-//import com.example.triptracker.view.profile.UserProfileFollowers
-//import com.example.triptracker.viewmodel.UserProfileViewModel
-//import io.github.kakaocup.compose.node.element.ComposeScreen
-//import io.mockk.Runs
-//import io.mockk.every
-//import io.mockk.impl.annotations.RelaxedMockK
-//import io.mockk.junit4.MockKRule
-//import io.mockk.just
-//import io.mockk.mockk
-//import org.junit.Before
-//import org.junit.Rule
-//import org.junit.Test
-//import org.junit.runner.RunWith
+// import androidx.compose.ui.test.junit4.createComposeRule
+// import androidx.lifecycle.MutableLiveData
+// import androidx.test.ext.junit.runners.AndroidJUnit4
+// import com.example.triptracker.model.profile.UserProfile
+// import com.example.triptracker.model.repository.UserProfileRepository
+// import com.example.triptracker.screens.userProfile.UserProfileFollowersScreen
+// import com.example.triptracker.view.Navigation
+// import com.example.triptracker.view.profile.UserProfileFollowers
+// import com.example.triptracker.viewmodel.UserProfileViewModel
+// import io.github.kakaocup.compose.node.element.ComposeScreen
+// import io.mockk.Runs
+// import io.mockk.every
+// import io.mockk.impl.annotations.RelaxedMockK
+// import io.mockk.junit4.MockKRule
+// import io.mockk.just
+// import io.mockk.mockk
+// import org.junit.Before
+// import org.junit.Rule
+// import org.junit.Test
+// import org.junit.runner.RunWith
 //
-//@RunWith(AndroidJUnit4::class)
-//class UserProfileFollowersTest {
+// @RunWith(AndroidJUnit4::class)
+// class UserProfileFollowersTest {
 //  @get:Rule val composeTestRule = createComposeRule()
 //
-//  // @get:Rule val instantTaskExecutorRule = InstantTaskExecutorRule()  // Ensure LiveData executes
+//  // @get:Rule val instantTaskExecutorRule = InstantTaskExecutorRule()  // Ensure LiveData
+// executes
 //  // immediately
 //
 //  @get:Rule val mockkRule = MockKRule(this)
@@ -60,6 +61,11 @@
 //            secondArg<(UserProfile?) -> Unit>().invoke(mockUserProfiles[6])
 //          }
 //      every { setListToFilter(any()) } just Runs
+//      every { mockUserProfileRepository.getAllUserProfiles(any()) } answers {
+//          // Invoke the callback with mock data
+//          val callback = arg<(List<UserProfile>) -> Unit>(0)
+//          callback(mockUserProfiles)
+//        }
 //    }
 //
 //    // Setting up the test composition
@@ -119,4 +125,4 @@
 //  //      }
 //  //    }
 //  //  }
-//}
+// }
