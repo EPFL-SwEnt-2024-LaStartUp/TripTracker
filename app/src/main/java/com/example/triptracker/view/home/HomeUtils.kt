c3853f26f5b36cc3c8e67aa706c2443187a1c1d4
Barghorn Jérémy <jeremy.barghorn@epfl.ch>
2024-05-16 12:34:41 +0200

    Add the possibility to delete a path the user created from the my trips view in the profile

--- app/src/main/java/com/example/triptracker/view/home/HomeUtils.kt
+++ app/src/main/java/com/example/triptracker/view/home/HomeUtils.kt
@@ -1,8 +1,10 @@
 package com.example.triptracker.view.home
 
 import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -33,7 +35,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -68,6 +72,7 @@ val dummyProfile = UserProfile("test@gmail.com", "Test User", "test", "test bio"
  * @param onClick: Function to call when the itinerary is clicked
  * @param test : Boolean to test the function
  */
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DisplayItinerary(
     itinerary: Itinerary,
@@ -75,7 +80,8 @@ fun DisplayItinerary(
     userProfileViewModel: UserProfileViewModel = viewModel(),
     onClick: () -> Unit,
     homeViewModel: HomeViewModel = viewModel(),
-    test: Boolean = false
+    test: Boolean = false,
+    canBeDeleted: Boolean = false
 ) {
   val configuration = LocalConfiguration.current
   val screenWidth = configuration.screenWidthDp.dp
@@ -138,6 +144,7 @@ fun DisplayItinerary(
           }
     }
     false -> {
+      val haptics = LocalHapticFeedback.current
       Box(
           modifier =
               Modifier.fillMaxWidth()
@@ -148,6 +155,14 @@ fun DisplayItinerary(
                     // overview with the selected itinerary highlighted and the first pinned places
                     onClick()
                   }
+                  .combinedClickable(
+                      onClick = { onClick() },
+                      onLongClick = {
+                        haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                        if (canBeDeleted) {
+                          homeViewModel.deleteItinerary(itinerary.id)
+                        }
+                      })
                   .testTag("Itinerary")) {
             Column(modifier = Modifier.fillMaxWidth().padding(25.dp)) {
               Row(
