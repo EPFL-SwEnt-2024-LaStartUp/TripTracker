d0add492b0facfbb820b87a7c7a725b1991411c1
loic <misentaloic@gmail.com>
2024-05-19 21:50:04 +0200

    rework StartScreen

--- app/src/main/java/com/example/triptracker/model/repository/ItineraryRepository.kt
+++ app/src/main/java/com/example/triptracker/model/repository/ItineraryRepository.kt
@@ -19,7 +19,7 @@ import com.google.firebase.firestore.firestore
 open class ItineraryRepository {
 
   // Initialise the Firebase Firestore
-  val db = Firebase.firestore
+  var db = Firebase.firestore
 
   // Reference to the collection of itineraries
   val itineraryDb = db.collection("itineraries")
