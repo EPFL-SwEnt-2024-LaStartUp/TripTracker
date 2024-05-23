e8c2cb37a9f30c74d116719cd5a09d702501cb86
cleo <cleo.renaud@epfl.ch>
2024-05-23 10:04:11 +0200

    Update UserProfileFollowing.kt

--- app/src/main/java/com/example/triptracker/view/profile/UserProfileFollowing.kt
+++ app/src/main/java/com/example/triptracker/view/profile/UserProfileFollowing.kt
@@ -1,38 +1,17 @@
 package com.example.triptracker.view.profile
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.triptracker.model.profile.MutableUserProfile
 import com.example.triptracker.model.profile.Relationship
 import com.example.triptracker.model.profile.UserProfile
@@ -40,7 +19,7 @@ import com.example.triptracker.view.Navigation
 import com.example.triptracker.view.NavigationBar
 import com.example.triptracker.view.profile.subviews.FriendListView
 import com.example.triptracker.view.profile.subviews.FriendSearchBar
-import com.example.triptracker.view.theme.Montserrat
+import com.example.triptracker.view.profile.subviews.ScaffoldTopBar
 import com.example.triptracker.viewmodel.UserProfileViewModel
 
 /**
@@ -76,41 +55,7 @@ fun UserProfileFollowing(
       userProfileViewModel.filteredUserProfileList.observeAsState(initial = emptyList())
 
   Scaffold(
-      topBar = {
-        Row(
-            modifier =
-                Modifier.height((LocalConfiguration.current.screenHeightDp * 0.075).dp)
-                    .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start) {
-              Button(
-                  onClick = { navigation.goBack() },
-                  colors =
-                      ButtonDefaults.buttonColors(
-                          containerColor = Color.Transparent,
-                          contentColor = MaterialTheme.colorScheme.onBackground),
-                  modifier = Modifier.testTag("GoBackButton")) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
-                  }
-
-              Text(
-                  text = "Following",
-                  style =
-                      TextStyle(
-                          fontSize = (LocalConfiguration.current.screenHeightDp * 0.03f).sp,
-                          lineHeight = (LocalConfiguration.current.screenHeightDp * 0.016f).sp,
-                          fontFamily = Montserrat,
-                          fontWeight = FontWeight(700),
-                          color = MaterialTheme.colorScheme.onBackground,
-                          textAlign = TextAlign.Left,
-                          letterSpacing = (LocalConfiguration.current.screenHeightDp * 0.0005f).sp,
-                      ),
-                  modifier =
-                      Modifier.width((LocalConfiguration.current.screenHeightDp * 0.67f).dp)
-                          .wrapContentHeight()
-                          .testTag("FollowingTitle"))
-            }
-      },
+      topBar = { ScaffoldTopBar(navigation = navigation, label = "Following") },
       bottomBar = { NavigationBar(navigation) },
       modifier = Modifier.fillMaxSize().testTag("FollowingScreen")) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding).testTag("FollowingList")) {
