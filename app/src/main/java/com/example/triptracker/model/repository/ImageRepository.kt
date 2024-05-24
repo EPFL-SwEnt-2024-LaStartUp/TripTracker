7c18ea80095bcd107c3341137e190bc619cb1183
Barghorn Jérémy <jeremy.barghorn@epfl.ch>
2024-05-24 17:21:12 +0200

    First test of reducing picture size

--- app/src/main/java/com/example/triptracker/model/repository/ImageRepository.kt
+++ app/src/main/java/com/example/triptracker/model/repository/ImageRepository.kt
@@ -1,11 +1,15 @@
 package com.example.triptracker.model.repository
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.util.Log
+import androidx.core.net.toFile
 import com.google.firebase.Firebase
 import com.google.firebase.storage.storage
 import java.util.UUID
 import kotlinx.coroutines.tasks.await
+import java.io.ByteArrayOutputStream
 
 /** Repository for interacting with Firebase Storage to upload and download images. */
 class ImageRepository {
@@ -27,12 +31,24 @@ class ImageRepository {
 
   suspend fun addImageToFirebaseStorage(folder: String, imageUri: Uri): Response<Uri> {
     return try {
+
+        val bitmap = BitmapFactory.decodeStream(imageUri.toFile().inputStream())
+
+        // Resize the Bitmap
+        val resizedBitmap = resizeBitmap(bitmap)
+
+        // Encode the resized Bitmap to a ByteArray
+        val byteArray = ByteArrayOutputStream().apply {
+            resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 80, this)
+        }.toByteArray()
+        
+        
       val downloadUrl =
           storage.reference
               .child(PICTURE_FOLDER)
               .child(folder)
               .child(UUID.randomUUID().toString())
-              .putFile(imageUri)
+              .putBytes(byteArray)
               .await()
               .storage
               .downloadUrl
@@ -44,4 +60,23 @@ class ImageRepository {
       Response.Failure(e)
     }
   }
+
+    private fun resizeBitmap(bitmap: Bitmap, maxWidth: Int = 800, maxHeight: Int = 800): Bitmap {
+        val width = bitmap.width
+        val height = bitmap.height
+
+        val aspectRatio = width.toFloat() / height.toFloat()
+        val newWidth: Int
+        val newHeight: Int
+
+        if (width > height) {
+            newWidth = maxWidth
+            newHeight = (newWidth / aspectRatio).toInt()
+        } else {
+            newHeight = maxHeight
+            newWidth = (newHeight * aspectRatio).toInt()
+        }
+
+        return Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true)
+    }
 }
