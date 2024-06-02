61d91828eb67a55405493764029385cfbb3d78e3
Pol <polinboston@gmail.com>
2024-06-02 17:32:56 +0200

    profille

--- app/src/androidTest/java/com/example/triptracker/e2eTest.kt
+++ app/src/androidTest/java/com/example/triptracker/e2eTest.kt
@@ -1,6 +1,9 @@
 package com.example.triptracker
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.triptracker.itinerary.MockItineraryList
 import com.example.triptracker.model.profile.MutableUserProfile
@@ -11,8 +14,10 @@ import com.example.triptracker.view.Navigation
 import com.example.triptracker.view.home.HomeScreen
 import com.example.triptracker.viewmodel.HomeViewModel
 import com.example.triptracker.viewmodel.UserProfileViewModel
+import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit4.MockKRule
+import io.mockk.mockk
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -22,50 +27,17 @@ import org.junit.runner.RunWith
 class E2ETest {
 
 
-    @get:Rule
-    val composeTestRule = createComposeRule()
-    @get:Rule
-    val mockkRule = MockKRule(this)
-
-    @RelaxedMockK
-    lateinit var mockNav: Navigation
-    @RelaxedMockK
-    private lateinit var mockViewModel: HomeViewModel
-    @RelaxedMockK
-    private lateinit var mockItineraryRepository: ItineraryRepository
-    // @RelaxedMockK private  lateinit var mockUserProfileRepository: UserProfileRepository
-    @RelaxedMockK
-    private lateinit var mockUserProfileRepository: UserProfileRepository
-    @RelaxedMockK
-    private lateinit var mockUserProfileViewModel: UserProfileViewModel
-    // private lateinit var mockUserProfileViewModel: UserProfileViewModel
-    @RelaxedMockK
-    private lateinit var mockProfile: MutableUserProfile
-
-    val mockList = MockItineraryList()
-    val mockItineraries = mockList.getItineraries()
-
-    val mockUserList = MockUserList()
-    val mockUsers = mockUserList.getUserProfiles()
-    val mockMail = "test@gmail.com"
 
 
     @Before
     fun setUp() {
 
+
     }
 
 
     @Test
-    fun navigateUserScreen(){
-
-        composeTestRule.setContent {
-            HomeScreen(
-                navigation = mockNav,
-                homeViewModel = mockViewModel,
-                userProfileViewModel = mockUserProfileViewModel,
-                test = true)
-        }
+    fun profileScreenFlow(){
 
 
 
