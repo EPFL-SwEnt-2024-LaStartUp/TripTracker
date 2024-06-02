8323057ec2c0d0c879cc2d569acb1ae0fb1d35a3
Pol <polinboston@gmail.com>
2024-06-02 17:15:14 +0200

    fun

--- app/src/androidTest/java/com/example/triptracker/e2eTest.kt
+++ app/src/androidTest/java/com/example/triptracker/e2eTest.kt
@@ -1,11 +1,22 @@
 package com.example.triptracker
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class E2ETest {
-    
+
+
+
+
+
+    @Test
+    fun navigateUserScreen(){
+
+
+    }
+
 
 
 
