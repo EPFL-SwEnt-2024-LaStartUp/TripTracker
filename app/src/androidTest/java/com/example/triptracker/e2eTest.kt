04771da0cc61407da137b602bf8506116d9530a0
UnavailableCode <leopold.galhaud@gmail.com>
2024-06-02 21:15:46 +0200

    End to end test
    Worked on the home screen part of the end to end test

--- app/src/androidTest/java/com/example/triptracker/e2eTest.kt
+++ app/src/androidTest/java/com/example/triptracker/e2eTest.kt
@@ -1,48 +1,129 @@
 package com.example.triptracker
 
+import android.content.Context
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.isDisplayed
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.test.espresso.action.ViewActions
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.triptracker.itinerary.MockItineraryList
+import com.example.triptracker.model.itinerary.Itinerary
 import com.example.triptracker.model.profile.MutableUserProfile
+import com.example.triptracker.model.profile.UserProfile
+import com.example.triptracker.model.repository.ItineraryRepository
+import com.example.triptracker.model.repository.UserProfileRepository
+import com.example.triptracker.screens.home.HomeViewScreen
+import com.example.triptracker.userProfile.MockUserList
 import com.example.triptracker.view.Navigation
 import com.example.triptracker.view.Route
+import com.example.triptracker.view.TopLevelDestination
 import com.example.triptracker.view.home.HomeScreen
-import com.example.triptracker.view.profile.UserProfileEditScreen
+import com.example.triptracker.view.map.MapOverview
 import com.example.triptracker.view.profile.UserProfileFavourite
 import com.example.triptracker.view.profile.UserProfileFriendsFinder
 import com.example.triptracker.view.profile.UserProfileMyTrips
 import com.example.triptracker.view.profile.UserProfileOverview
 import com.example.triptracker.view.profile.UserProfileSettings
+import com.example.triptracker.viewmodel.HomeViewModel
+import com.example.triptracker.viewmodel.MapViewModel
+import com.example.triptracker.viewmodel.UserProfileViewModel
+import io.github.kakaocup.compose.node.element.ComposeScreen
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit4.MockKRule
+import io.mockk.mockk
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class E2ETest {
+  private val appContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
 
   @get:Rule val composeTestRule = createComposeRule()
 
-  var profile = MutableUserProfile()
+  // This rule automatic initializes lateinit properties with @MockK, @RelaxedMockK, etc.
+  @get:Rule val mockkRule = MockKRule(this)
 
-  /*
-     Test to check the flow of the profile screen navigating through different screens
-  */
+  // Relaxed mocks methods have a default implementation returning values
+
+  @RelaxedMockK lateinit var mockNav: Navigation
+  @RelaxedMockK private lateinit var homeViewModel: HomeViewModel
+  @RelaxedMockK private lateinit var mapViewModel: MapViewModel
+
+  @RelaxedMockK private lateinit var mockItineraryRepository: ItineraryRepository
+  @RelaxedMockK private lateinit var mockUserProfileRepository: UserProfileRepository
+  @RelaxedMockK private lateinit var mockUserProfileViewModel: UserProfileViewModel
+  @RelaxedMockK private lateinit var mockProfile: MutableUserProfile
+  var profile = MutableUserProfile()
+  val mockList = MockItineraryList()
+  val mockItineraries = mockList.getItineraries()
+
+  val mockUserList = MockUserList()
+  val mockUsers = mockUserList.getUserProfiles()
+  val mockMail = "test@gmail.com"
+
+  @Before
+  fun setUp() {
+    // Mocking necessary components
+    mockNav = mockk(relaxed = true)
+    mockItineraryRepository = mockk(relaxed = true)
+    homeViewModel = mockk(relaxed = true)
+    mapViewModel = mockk(relaxed = true)
+    // This allows mocking unit returning functions
+    mockUserProfileRepository = mockk(relaxUnitFun = true)
+    mockUserProfileViewModel = mockk(relaxUnitFun = true)
+
+    MockKAnnotations.init(this, relaxUnitFun = true)
+    mockProfile = mockk(relaxUnitFun = true)
+
+    // Log.d("ItineraryList", mockViewModel.itineraryList.value.toString())
+    every { mockNav.getTopLevelDestinations()[0] } returns
+        TopLevelDestination(Route.HOME, Icons.Outlined.Home, "Home")
+
+    every { mockUserProfileViewModel.fetchAllUserProfiles { any() } } answers
+        {
+          // Invoke the callback with mock data
+          val callback = arg<(List<UserProfile>) -> Unit>(0)
+          callback(mockUsers)
+        }
+  }
+
   @Test
   fun profileScreenFlow() {
+
+    every { mockUserProfileRepository.getUserProfileByEmail(mockMail) {} } returns
+        mockk(relaxUnitFun = true)
+    // this removes previous problem
+    every { mockItineraryRepository.getAllItineraries(any()) } answers
+        {
+          // Invoke the callback with mock data
+          val callback = arg<(List<Itinerary>) -> Unit>(0)
+          callback(mockItineraries)
+        }
+    every { homeViewModel.itineraryList } returns MutableLiveData(mockItineraries)
+    every { homeViewModel.filteredItineraryList } returns MutableLiveData(mockItineraries)
+    every { mockProfile.userProfile.value } returns mockUsers[0]
 
     composeTestRule.setContent {
       Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
@@ -50,9 +131,11 @@ class E2ETest {
         val navigation = remember(navController) { Navigation(navController) }
         NavHost(
             navController = navController,
-            startDestination = Route.PROFILE,
+            startDestination = Route.HOME,
         ) {
-          composable(Route.HOME) { HomeScreen(navigation) }
+          composable(Route.HOME) {
+            HomeScreen(navigation = navigation, homeViewModel = homeViewModel, test = true)
+          }
           composable(Route.FRIENDS) {
             UserProfileFriendsFinder(navigation = navigation, profile = profile)
           }
@@ -65,8 +148,13 @@ class E2ETest {
                 userProfile = profile,
             )
           }
-          composable(Route.EDIT) {
-            UserProfileEditScreen(navigation = navigation, profile = profile)
+          composable("MAPS?id={id}", arguments = listOf(navArgument("id") { defaultValue = "" })) {
+              backStackEntry ->
+            MapOverview(
+                context = appContext,
+                navigation = navigation,
+                selectedId = backStackEntry.arguments?.getString("id") ?: "",
+                userProfile = profile)
           }
           composable(Route.SETTINGS) { UserProfileSettings(navigation) }
           composable(Route.FAVORITES) {
@@ -75,6 +163,58 @@ class E2ETest {
         }
       }
     }
+
+    composeTestRule.onNodeWithTag("HomeScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("ItineraryList").assertIsDisplayed()
+    ComposeScreen.onComposeScreen<HomeViewScreen>(composeTestRule) {
+      // Test the UI elements
+      composeTestRule
+          .onNodeWithText("Find Itineraries", useUnmergedTree = true)
+          .assertIsDisplayed()
+          .assertTextEquals("Find Itineraries")
+
+      profilePic {
+        assertIsDisplayed()
+        performClick()
+      }
+
+      profilePicScreen { assertIsDisplayed() }
+
+      profilePicClose {
+        assertIsDisplayed()
+        performClick()
+      }
+      searchBar {
+        assertIsDisplayed()
+        composeTestRule.onNodeWithTag("searchBarText", useUnmergedTree = true).assertIsDisplayed()
+        composeTestRule
+            .onNodeWithTag("searchBarText", useUnmergedTree = true)
+            .assertTextEquals("Find Itineraries")
+        performClick()
+        composeTestRule.waitForIdle()
+        ViewActions.pressKey(84) // letter t, doesn't update the UI yet
+
+        // Wait for the UI to update with the LiveData change
+        composeTestRule.waitForIdle()
+
+        composeTestRule.onNodeWithTag("BackButton", useUnmergedTree = true).performClick()
+        performClick()
+        composeTestRule.onNodeWithTag("ClearButton", useUnmergedTree = true).performClick()
+      }
+      itineraryUsernameBox {
+        assertIsDisplayed()
+        assertHasClickAction()
+        Log.d("Itinerary", "username is displayed and clickable")
+      }
+      itinerary {
+        assertIsDisplayed()
+        assertHasClickAction()
+        Log.d("Itinerary", "Itinerary is displayed and clickable")
+        performClick()
+      }
+    }
+
+    composeTestRule.onNodeWithText("Maps").performClick()
 
     composeTestRule.onNodeWithTag("ProfileOverview").assertIsDisplayed()
 
@@ -117,20 +257,6 @@ class E2ETest {
 
     // verify that the my trips screen is open
     composeTestRule.onNodeWithTag("UserProfileMyTripsScreen").assertIsDisplayed()
-
-    // Go back and assert we are in the profile screen
-    composeTestRule.onNodeWithTag("GoBackButton").performClick()
-    composeTestRule.onNodeWithTag("ProfileOverview").assertIsDisplayed()
-
-    // Edit profile
-    composeTestRule.onNodeWithContentDescription("Edit").assertHasClickAction()
-    composeTestRule.onNodeWithContentDescription("Edit").performClick()
-
-    // verify that the edit screen is open
-    composeTestRule.onNodeWithTag("UserProfileEditScreen").assertIsDisplayed()
-
-    // input into the field
-    composeTestRule.onNodeWithText("Save").isDisplayed()
 
     // Go back and assert we are in the profile screen
     composeTestRule.onNodeWithTag("GoBackButton").performClick()
