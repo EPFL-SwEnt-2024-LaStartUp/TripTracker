1055a74964560197fae0c903a2ef5dfdd123c198
Pol <polinboston@gmail.com>
2024-06-02 18:11:05 +0200

    working nav test

--- app/src/androidTest/java/com/example/triptracker/e2eTest.kt
+++ app/src/androidTest/java/com/example/triptracker/e2eTest.kt
@@ -1,11 +1,23 @@
 package com.example.triptracker
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Place
+import androidx.compose.material.icons.outlined.RadioButtonChecked
+import androidx.compose.runtime.remember
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.triptracker.itinerary.MockItineraryList
 import com.example.triptracker.model.network.Connection
@@ -14,9 +26,23 @@ import com.example.triptracker.model.profile.UserProfile
 import com.example.triptracker.model.repository.ItineraryRepository
 import com.example.triptracker.model.repository.UserProfileRepository
 import com.example.triptracker.userProfile.MockUserList
+import com.example.triptracker.view.LoginScreen
 import com.example.triptracker.view.Navigation
+import com.example.triptracker.view.OfflineScreen
+import com.example.triptracker.view.Route
+import com.example.triptracker.view.TopLevelDestination
 import com.example.triptracker.view.home.HomeScreen
+import com.example.triptracker.view.map.MapOverview
+import com.example.triptracker.view.map.RecordScreen
+import com.example.triptracker.view.profile.UserProfileEditScreen
+import com.example.triptracker.view.profile.UserProfileFavourite
+import com.example.triptracker.view.profile.UserProfileFollowers
+import com.example.triptracker.view.profile.UserProfileFollowing
+import com.example.triptracker.view.profile.UserProfileFriendsFinder
+import com.example.triptracker.view.profile.UserProfileMyTrips
 import com.example.triptracker.view.profile.UserProfileOverview
+import com.example.triptracker.view.profile.UserProfileSettings
+import com.example.triptracker.view.profile.UserView
 import com.example.triptracker.viewmodel.HomeViewModel
 import com.example.triptracker.viewmodel.UserProfileViewModel
 import io.mockk.MockKAnnotations
@@ -38,6 +64,10 @@ class E2ETest {
     @RelaxedMockK private lateinit var homevm: HomeViewModel
     @RelaxedMockK private lateinit var mockNavigation: Navigation
     @RelaxedMockK private lateinit var connection: Connection
+    val profile = MutableUserProfile()
+
+
+    //setup navhostcontroller
 
     @Before
     fun setup() {
@@ -46,9 +76,19 @@ class E2ETest {
         userProfilevm = mockk(relaxed = true)
         homevm = mockk(relaxed = true)
         connection = mockk(relaxed = true)
+
+
+        every { mockNavigation.getTopLevelDestinations() } returns
+                listOf(
+                    TopLevelDestination(Route.HOME, Icons.Outlined.Home, "Home"),
+                    TopLevelDestination(Route.MAPS, Icons.Outlined.Place, "Maps"),
+                    TopLevelDestination(Route.RECORD, Icons.Outlined.RadioButtonChecked, "Record"),
+                    TopLevelDestination(Route.PROFILE, Icons.Outlined.Person, "Profile"),
+                )
+
+
+
     }
-
-
 
 
 
@@ -66,18 +106,31 @@ class E2ETest {
             every { onConnectionRefresh() } returns true
             every { setProfile(any()) } returns Unit
         }
-
         composeTestRule.setContent {
-            UserProfileOverview(
-                profile = MutableUserProfile(),
-                navigation = mockNavigation,
-                homeViewModel = homevm,
-                userProfileViewModel = userProfilevm)
+            val navController = rememberNavController()
+            val navigation = remember(navController) { Navigation(navController) }
+            NavHost(
+                navController = navController,
+                startDestination = Route.PROFILE,
+            ) {
+                composable(Route.HOME) { HomeScreen(navigation) }
+                composable(Route.FRIENDS) {
+                    UserProfileFriendsFinder(navigation = navigation, profile = profile)
+                }
+                composable(Route.PROFILE) {
+                    UserProfileOverview(navigation = navigation, profile = profile)
+                }
+            }
+
         }
 
         composeTestRule.onNodeWithTag("ProfileOverview").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("FavoritesButton").assertHasClickAction()
-        composeTestRule.onNodeWithTag("FavoritesButton").performClick()
+        composeTestRule.onNodeWithTag("FriendsButton").assertHasClickAction()
+        composeTestRule.onNodeWithTag("FriendsButton").performClick()
+
+        //verify that the favorites screen is open
+
+
 
 
 
